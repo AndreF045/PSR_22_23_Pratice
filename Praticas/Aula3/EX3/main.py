1efa25579be53d7f944f06c1a06c6189e3b2dece
AndreF045 <andreferreiraa045@gmail.com>
2022-10-06 11:42:43 +0100

    aula 3

---
+++ Praticas/Aula3/EX3/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from time import time, ctime
+import argparse
+import colorama 
+
+def main():
+    print("depois ver aula para percerber")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
