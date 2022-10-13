5483dab77dc33cba81e22071dbf9ad01aae43069
AndreF045 <andreferreiraa045@gmail.com>
2022-10-13 11:02:29 +0100

    aula 4

--- Praticas/Aula4/Exe1/main.py
+++ Praticas/Aula4/Exe1/main.py
@@ -6,11 +6,8 @@ def main():
 
     img = cv2.imread("/home/andre/Pictures/green_man.jpeg")
 
-    if img is None:
-        sys.exit("Could not read the image.")
-
-    cv2.imshow("este é o shek", img)
-    k = cv.waitKey(0)
+    cv2.imshow("shrek", img)
+    k = cv2.waitKey(0)
 
     if k == ord("s"):
         cv2.imwrite("shrek.png", img)
