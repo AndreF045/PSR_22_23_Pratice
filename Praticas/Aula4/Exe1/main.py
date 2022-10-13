009f9da82de41841a64cb5c5741b93838e1edc71
AndreF045 <andreferreiraa045@gmail.com>
2022-10-13 11:29:19 +0100

    aula 4

--- Praticas/Aula4/Exe1/main.py
+++ Praticas/Aula4/Exe1/main.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 
+from ast import parse
+from email import parser
 import cv2
+import argparse
+
 
 def main():
 
-    img = cv2.imread("/home/andre/Pictures/green_man.jpeg")
 
-    cv2.imshow("shrek", img)
-    k = cv2.waitKey(0)
+    parser=argparse.ArgumentParser(description="path to image")
+    parser.add_argument("-img", "--img_path", help = "mode: time", default= "/home/andre/Desktop/PSR/PSR_22_23_Pratice/Praticas/Aula4/images/atlascar2.png")
+    args = parser.parse_args()
 
-    if k == ord("s"):
-        cv2.imwrite("shrek.png", img)
+    image_1 = cv2.imread(args.img_path, cv2.IMREAD_COLOR)
+    image_2 = cv2.imread("/home/andre/Desktop/PSR/PSR_22_23_Pratice/Praticas/Aula4/images/atlascar.png", cv2.IMREAD_COLOR)
+
+    cv2.imshow("atlas", image_1)
+    cv2.imshow("atlas", image_2)
+    cv2.waitKey(0)
+
 
   
 if __name__ == '__main__':
