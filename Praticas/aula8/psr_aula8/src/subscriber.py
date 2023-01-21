a4e49695bf97dd62f7deef8d91ac77eb4b554f71
AndreF045 <andrerferreira@ua.pt>
2023-01-21 18:02:08 +0000

    8

---
+++ Praticas/aula8/psr_aula8/src/subscriber.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def msgReceivedCallback(msg):
+    rospy.loginfo('Message received: ' + str(msg) + '\n')
+
+
+def main():
+
+    #.........................................
+    #initialize the node
+    #.........................................
+
+    parser = argparse.ArgumentParser(description=' ')
+    parser.add_argument('-t', '--topic', type=str, required=False, default='chatter')
+    args = vars(parser.parse_args())
+
+    #initilize the node ros
+
+    rospy.init_node('subscriber', anonymous=True)
+
+    #INNITIALIZE THE SUBSCRIBER
+    rospy.Subscriber(args['topic'], Dog, msgReceivedCallback)
+
+    #.........................................
+    #execute the node
+    #.........................................
+    rospy spin()
+
+    #.........................................
+    #terminate the node
+    #.........................................
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
